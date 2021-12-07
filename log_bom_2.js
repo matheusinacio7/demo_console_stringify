ed3fc7e046ccab0380b56a9949f8448051b149c3
heyset <c.inaciomatheus@gmail.com>
2021-12-07 15:51:09 -0300

    Adiciona outro caso de log

---
+++ log_bom_2.js
@@ -0,0 +1,33 @@
+const firstDriver = {
+  name: {
+    firstName: 'Janete',
+    lastName: 'Corça',
+  },
+  dob: {
+    date: {
+      year: 1989,
+      month: 4,
+      day: 12,
+    },
+    age: 32,
+  },
+};
+
+const secondDriver = {
+  name: {
+    firstName: 'João',
+    lastName: 'Corça'
+  },
+  dob: {
+    date: {
+      year: 1992,
+      month: 8,
+      day: 23,
+    },
+    age: 29,
+  },
+};
+
+const drivers = [firstDriver, secondDriver];
+
+console.log(JSON.stringify(drivers, null, '\t'));
